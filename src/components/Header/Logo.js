8856d160e0413a03413112df6a8921586993b72e
Ronique Ricketts <johnoyrick@gmail.com>
2018-11-04 06:40:53 -0500

    added search bar to navigation bar

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import { Image } from 'semantic-ui-react'
 
-import moltin from '../../images/moltin-light-hex.svg.svg'
-
 const Logo = () => (
-  <Image
-    size="mini"
-    src={moltin}
-    style={{ marginRight: '1.5em' }}
-    alt="I love Lamp"
-  />
+  <Image src='../images/header.png' avatar />
 )
 
 export default Logo
